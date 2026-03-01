046b8e864c8b26a1cb6af6ae768e9c2f3efc0239
Mona_Ahmed_Eid <monaahmedeidramadan2007@gmail.com>
2026-03-01 23:50:40 +0200

    add student.cpp

---
+++ course.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include "student.h"
+#include "course.h"
+
+using namespace std;
+
+void addCourse(std::vector<Course>& courses) {
+
+    Course new_Course;
+
+    cout << "----------------------------< ADD NEW COURSE >-------------------------------\n";
+    cout << "Enter Course ID (Ex:CS101): ";
+    cin >> new_Course.id;
+
+    if (findCourseById(courses, new_Course.id) != nullptr) {
+        cout << " Error: Course with code ( " << new_Course.id << " ) already exists!\n";
+        return;
+    }
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Enter Course Title: ";
+    getline(cin, new_Course.title);
+
+    cout << "Enter Credit Hours: ";
+    while (!(cin >> new_Course.credit_hours) || new_Course.credit_hours <= 0 ||new_Course.credit_hours >4) {
+        cout << "Invalid input!\n"<<"Please , Enter valid Credit hours: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    courses.push_back(new_Course);
+    cout << "____________________________________________________________________________\n";
+    cout << "                       COURSE ADDED SUCCESSFULLY                            \n";
+    cout << "____________________________________________________________________________\n";
+}
+//-----------------find course && tell user if he is not found---------------
+Course* findCourseById(vector<Course>& courses, const string& id) {
+    for (auto& c : courses) {
+        if (c.id == id) {
+            return &c;
+        }
+    }
+    return nullptr;
+}
