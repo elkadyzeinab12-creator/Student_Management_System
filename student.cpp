ca96fe2b6ccb25d5e41ad6e351c92610f6a813fd
Mona_Ahmed_Eid <monaahmedeidramadan2007@gmail.com>
2026-03-01 03:47:30 +0200

    add two functions edit and delete

--- student.cpp
+++ student.cpp
@@ -58,3 +58,71 @@ Student* findStudentById(vector<Student>& students, const string& id) {
             return &s;
     return nullptr;
 }
+//------------------------------Delete student -----------------------------------
+void deleteStudent(vector<Student>& students) {
+    string id;
+    cout << "Enter Student ID to delete: ";
+    cin >> id;
+    Student* studentPtr = findStudentById(students, id);
+    if (studentPtr != nullptr) {
+        students.erase(students.begin() + (studentPtr - &students[0]));//to get index and do erase
+        cout << "_____________________________________________________________________________\n";
+        cout << "                       STUDENT DELETED SUCCESSFULLY                        \n";
+        cout << "_____________________________________________________________________________\n";
+    }else {
+        cout << "Error: Student with ID ( " << id << " ) was not found!\n";
+    }
+}
+//----------------------------------Edit student info-------------------------------
+void editStudent(vector<Student>& students) {
+    string id;
+    cout << "Enter Student ID to edit: ";
+    cin >> id;
+    Student* sPtr = findStudentById(students, id);
+    if (sPtr == nullptr) {
+        cout << " Error: Student with ID ( " << id << " ) was not found!\n";
+        return;
+    }
+    int choice;
+    do {
+        cout << "\n-----------------------< EDIT STUDENT DATA >------------------------\n";
+        cout << "Current Data < Name: " << sPtr->name << " > < Year: " << sPtr->year << " >\n";
+        cout << "----------------------------------------------------------------------\n";
+        cout << "1. Edit Name\n";
+        cout << "2. Edit Academic Year\n";
+        cout << "3. Save & Exit\n";
+        cout << "Enter your choice: ";
+        while (!(cin >> choice)) {
+            cout << "Invalid input! Please enter a number (1-3): ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        switch (choice) {
+            case 1:
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Enter New Name: ";
+                getline(cin, sPtr->name);
+                cout << "Name updated successfully!\n";
+                break;
+            case 2:
+                cout << "Enter New Academic Year: ";
+                while (!(cin >> sPtr->year)) {
+                    cout << "Invalid! Please enter a number for Year: ";
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                cout << "Year updated successfully!\n";
+                break;
+            case 3:
+                cout << "____________________________________________________________________\n";
+                cout << "                       CHANGES SAVED SUCCESSFULLY                   \n";
+                cout << "____________________________________________________________________\n";
+                break;
+
+            default:
+                cout << "Invalid choice! Please select 1, 2 or 3\n";
+        }
+    } while (choice != 3);
+}
+//---------------------------------Calculate GPA----------------------------------
+
